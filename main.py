145f2b858b466f64bb80622972ea4ea4e3ce48eb
luohongk <luohongkun@whu.edu.cn>
2025-04-15 12:55:13 +0800

    Fix typo in keyword list.

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ with open("README.md", "r") as f:
 logging.info("获取关键词列表")
 
 keywords = [
-    "Vision-and-Language Navigation",
+    "Vision and Language Navigation",
     "Vision Language Action",
     "Embodied AI",
     "Visual SLAM",
