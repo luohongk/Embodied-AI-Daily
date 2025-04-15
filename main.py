ea149e6f2fe9bb78067c484cf3ec1503135e8789
luohongk <luohongkun@whu.edu.cn>
2025-04-15 13:04:34 +0800

    Remove 'Embodied AI' and 'MVS' from keyword list

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ logging.info("获取关键词列表")
 keywords = [
     "Vision and Language Navigation",
     "Vision Language Action",
-    "Embodied AI",
     "Visual SLAM",
     "Visual Inertial SLAM",
     "Visual Inertial Odometry",
@@ -58,7 +57,6 @@ keywords = [
     "Loop Closure Detection",
     "Visual Place Recognition",
     "3D Gaussian Splatting",
-    "MVS",
     "Deep Learning",
     "LLM",
 ]  # TODO add more keywords
